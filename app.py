5eff27fcee2be4e5b2cea2df42a6f9317efa103c
nortonlyr <nortonlyr@gmail.com>
2020-08-05 00:06:42 -0400

    App.py added

--- app.py
+++ app.py
@@ -6,3 +6,21 @@ app = flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', method=['POST'])
+def predict():
+    """
+    Rendering results on the HTML GUI
+    """
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(finale_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text = 'Employee Salary should be $ {}'.format(output))
+
+if __name__ == "__main__":
+    app.tun(debug=True)
